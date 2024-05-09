aa937f27c02d2e5e76906f2e0b531587dda8ed7a
mohamed rafik <114211230+mohamedrafik-lgtm@users.noreply.github.com>
2024-05-09 06:38:06 +0300

    Delete Tab Method And Last Tab Activ

--- src/components/OpenedFileBarIcon.tsx
+++ src/components/OpenedFileBarIcon.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { IFile } from "../interfaces"
 import RenderFileIcon from "./RenderFileIcon"
 import { CloseIcon } from "./svg/ClosIcon"
-import { setClickedFile } from "../app/retuers/TreeSlice"
+import { setClickedFile, setOpenedFile } from "../app/retuers/TreeSlice"
 import { RootState } from "../app/store"
 
 interface IProp{
@@ -11,12 +11,17 @@ file:IFile,
 
 const OpenedFileBarIcon = ({file}:IProp) => {
     const dispatch = useDispatch()
-    const {clickedFile:{activTabId}} = useSelector((state:RootState) => state.tree)
+    const {openedFile,clickedFile:{activTabId}} = useSelector((state:RootState) => state.tree)
     // handlers
     const onClick = () => {
         const {id,name,content} = file
         dispatch(setClickedFile({fileContent:content,fileName:name,activTabId:id}))
-        
+    }
+    const onRemove = (selictedId:string) =>{
+        const filtered = openedFile.filter(file => file.id !== selictedId)
+        const {id,name,content} = filtered[filtered.length - 1]
+        dispatch(setOpenedFile(filtered))
+        dispatch(setClickedFile({activTabId:id,fileContent:content,fileName:name}))
     }
     return (
         <div className={`flex item-center p-2 border-t-2 ${file.id === activTabId ? "border-[#cf6ccf]" : "border-transparent"}`} onClick={onClick}
@@ -25,7 +30,11 @@ const OpenedFileBarIcon = ({file}:IProp) => {
             <span className="cursor-pointer duration-300 flex justify-center item-center wx-2 mr-2 p-1 rounded-md" >
         {file.name}
             </span>
-            <span className="cursor-pointer duration-300 flex justify-center item-center wx-2 mr-2 p-1 rounded-md"> 
+            <span className="cursor-pointer duration-300 flex justify-center item-center wx-2 mr-2 p-1 rounded-md"
+            onClick={e =>{
+                    e.stopPropagation()
+                    onRemove(file.id)
+            }}> 
                 <CloseIcon/>
             </span>
         </div>
